3cfe6e6eb884c761937be1e3764474d5cbf26c43
vinicius <viniciusdosanjos63@gmail.com>
2025-09-02 20:31:01 -0300

    project bootstrap

---
+++ Controllers/CreateExam.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SirespFacil.Controllers
+{
+    public class CreateExam : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
