72a546de9a298893749f4a125a7e17610510bba7
DudaaMoura <101333153+DudaaMoura@users.noreply.github.com>
2025-09-16 20:40:31 -0300

    Concluindo Uma Parte Do Design Do Formulario

--- obj/Debug/net8.0/SirespFacil.AssemblyInfo.cs
+++ obj/Debug/net8.0/SirespFacil.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SirespFacil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb059de8c46559f418123b9a2d6f0416eeb81b1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee9fb7f896a7bf6eae784d810816ae31884627f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SirespFacil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SirespFacil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
