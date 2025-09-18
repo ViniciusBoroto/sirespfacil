764ceeb4c9a903d5a595122fd60d1e481c43f25a
vinicius <viniciusdosanjos63@gmail.com>
2025-09-18 19:46:46 -0300

    dbsets on app context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿namespace SirespFacil.Data
+{
+    // Data/AppDbContext.cs
+    using Microsoft.EntityFrameworkCore;
+    using SirespFacil.Models;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<RessonanciaMagnetica> RessonanciasMagneticas { get; set; }
+        public DbSet<Conduta> Condutas { get; set; }
+
+        public DbSet<CriterioDeAutorizacao> CriteriosDeAutorizacao { get; set; }
+
+        public DbSet<Exame> Exames { get; set; }
+
+        public DbSet<ExameSolicitado> ExameSolicitados { get; set; }
+
+        public DbSet<ImplanteMetalico> ImplantesMetalicos { get; set; }
+        public DbSet<Justificativa> Justificativas { get; set; }
+        public DbSet<Lateralidade> Lateralidades { get; set; }
+        public DbSet<Medico> Medicos { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Sexo> Sexo { get; set; }
+        public DbSet<Solicitante> Solicitantes { get; set; }
+        public DbSet<TipoCriterioAutorizacao> TiposCriterioAutorizacao { get; set; }
+        public DbSet<TipoExame> TiposExames { get; set; }
+        public DbSet<Unidade> Unidades { get; set; }
+    }
+}
---
+++ Repositories/RessonanciaMagneticaRepository.cs
@@ -0,0 +1,31 @@
+﻿using SirespFacil.Models;
+using System;
+
+namespace SirespFacil.Repositories
+{
+    public class RessonanciaMagneticaRepository
+    {
+            private readonly AppDbContext _context;
+
+            public RessonanciaMagneticaRepository(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public IEnumerable<RessonanciaMagnetica> GetAll()
+            {
+                return _context.Exames.ToList();
+            }
+
+            public Exame GetById(int id)
+            {
+                return _context.Exames.Find(id);
+            }
+
+            public void Add(Exame exame)
+            {
+                _context.Exames.Add(exame);
+                _context.SaveChanges();
+            }
+        }
+}
